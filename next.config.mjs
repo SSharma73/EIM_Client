e02f29cff45572b1fce6fe9d5693fe266e5a1819
SSharma73 <101551011+SSharma73@users.noreply.github.com>
2024-10-28 18:12:52 +0530

    changes done

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    // NEXTAUTH_URL: "https://admin.eim.digital/",
+    NEXTAUTH_URL: "https://admin.eim.digital/",
     // NEATH_URL: "http://localhost:3000",
     NEXTAUTH_SECRET: "Vx4GgzAwkhQXxwL3r3a0rcbxgF63Rmp2ke11yd5K8dY=",
   },
