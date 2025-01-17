b3ec08210290ed17983d041bb400e0263893a5f4
SSharma73 <101551011+SSharma73@users.noreply.github.com>
2025-01-17 17:01:37 +0530

    admin path

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     NEXTAUTH_URL: "https://admin.eim.digital/",
-    NEATH_URL: "http://localhost:3000",
+    // NEATH_URL: "http://localhost:3000",
     NEXTAUTH_SECRET: "Vx4GgzAwkhQXxwL3r3a0rcbxgF63Rmp2ke11yd5K8dY=",
   },
   async redirects() {
